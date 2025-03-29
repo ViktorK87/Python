ad4629a414344257db06949610e4873b21074883
Виктор <140902080+ViktorK87@users.noreply.github.com>
2025-03-29 10:11:21 +0300

    Update test_03_shop.py

--- test_03_shop.py
+++ test_03_shop.py
@@ -1,4 +1,3 @@
-import pytest
 import re
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -11,16 +10,22 @@ driver = webdriver.Chrome(
 
 driver.get("https://www.saucedemo.com/")
 
-input_user = driver.find_element(By.CSS_SELECTOR, (".input_error[placeholder='Username']"))
-input_pswrd = driver.find_element(By.CSS_SELECTOR, (".input_error[placeholder='Password']"))
+input_user = driver.find_element(By.CSS_SELECTOR,
+                                 (".input_error[placeholder='Username']")
+                                 )
+input_pswrd = driver.find_element(By.CSS_SELECTOR,
+                                  (".input_error[placeholder='Password']")
+                                  )
 btn_login = driver.find_element(By.CSS_SELECTOR, ('.submit-button'))
 
 input_user.send_keys("standard_user")
 input_pswrd.send_keys("secret_sauce")
 btn_login.click()
 
-driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
+driver.find_element(By.CSS_SELECTOR,
+                    "#add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.CSS_SELECTOR,
+                    "#add-to-cart-sauce-labs-bolt-t-shirt").click()
 driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
 driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link").click()
 driver.find_element(By.CSS_SELECTOR, "#checkout").click()
@@ -31,7 +36,12 @@ driver.find_element(By.CSS_SELECTOR, "#continue").click()
 total = driver.find_element(By.CSS_SELECTOR, '[data-test="total-label"]').text
 driver.close()
 
-last_total = "$" + re.findall(r'\d+', total)[0] + "." + re.findall(r'\d+', total)[1]
+last_total = "$" + re.findall(r'\d+', total)[0] + \
+             "." + re.findall(r'\d+', total)[1]
+
 
 def test_total_summ():
     assert last_total == "$58.29"
+
+
+driver.close()
