4e566661a9f09969708b6213e3bb820139b2d55f
Виктор <140902080+ViktorK87@users.noreply.github.com>
2025-04-28 00:45:48 +0300

    Update new_test_projects.py

--- new_test_projects.py
+++ new_test_projects.py
@@ -1,8 +1,8 @@
 from new_page_company import New_page_company
 
 name = "Поток_100"
-company_id = "ae23443f-caa3-49bb-aad9-78810990b1a1"
-bearer = "Bearer DnPNWaWclaanUqec0sABF6Yi4HX5ERrijbd2SC0Z5wzOYDTsf5DUyhc2Rm9q460h"
+company_id = 
+bearer = 
 company = New_page_company(company_id, bearer)
 
 def test_post_project():
